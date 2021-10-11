0c14e166ec8200b789c3f89c27f23d894f79e26b
elderopedia <loicmagny60@gmail.com>
2021-10-11 21:34:00 +0200

    first commit ?

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// App.js
+
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/home.js";
+import About from "./components/about.js";
+import Contact from "./components/contact.js";
+
+class App extends Component {
+	render() {
+		return (
+			<Router>
+				<div>
+					<h2>Bienvenue</h2>
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<ul className="navbar-nav mr-auto">
+							<li>
+								<Link to={"/"} className="nav-link">
+									{" "}
+									Accueil{" "}
+								</Link>
+							</li>
+							<li>
+								<Link to={"/contact"} className="nav-link">
+									Contact
+								</Link>
+							</li>
+							<li>
+								<Link to={"/about"} className="nav-link">
+									A propos
+								</Link>
+							</li>
+						</ul>
+					</nav>
+					<hr />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/contact" component={Contact} />
+						<Route path="/about" component={About} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
+}
+
+export default App;
