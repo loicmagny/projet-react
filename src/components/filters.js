74bbcacf82ecd972e4cf129199d176ecdbd55d85
elderopedia <loicmagny60@gmail.com>
2021-11-01 16:49:40 +0100

    début merge + travail filtres

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-export default function FiltersProductList()
-{
-    return(
-        <div className="filters">
-            <ul className="filters__list">
-                <li className="filters__list__item">Marques</li>
-                <li className="filters__list__item">Prix</li>
-                <li className="filters__list__item">Type</li>
-                <li className="filters__list__item">RGB</li>
-            </ul>
-        </div>
-    )
+export default function FiltersProductList() {
+	return (
+		<div className="filters">
+			<ul className="filters__list">
+				<li className="filters__list__item">Marques</li>
+				<li className="filters__list__item">Prix</li>
+				<li className="filters__list__item">Type</li>
+				<li className="filters__list__item">RGB</li>
+			</ul>
+		</div>
+	);
 }
